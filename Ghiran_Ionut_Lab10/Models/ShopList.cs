c89ab7e6abeca4b84321138e5e6bb2c8f5693c75
Ghiranu <ionutgrn@yahoo.com>
2022-01-15 19:19:48 +0200

    Add project files.

---
+++ Ghiran_Ionut_Lab10/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SQLite;
+
+namespace Ghiran_Ionut_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
